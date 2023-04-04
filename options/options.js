75532f03577e662e21a6c000d0f4d36ea0f68eb5
Crowdbotics <team@crowdbotics.com>
2023-04-04 20:02:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-credit-39711",
+  url: "https://jolly-credit-39711.botics.co",
+  api: "https://jolly-credit-39711.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
