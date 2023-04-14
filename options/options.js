3760342a7b841f83280b826068e2d4a6c697eacd
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-04-14 20:04:40 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "jolly-credit-39711",
-  url: "https://jolly-credit-39711.botics.co",
-  api: "https://jolly-credit-39711.botics.co/api/v1"
-}
+  "name": "jolly-credit-39711",
+  "url": "https://jolly-credit-39711.botics.co",
+  "api": "https://jolly-credit-39711.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
